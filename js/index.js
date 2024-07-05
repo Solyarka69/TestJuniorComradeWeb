32c95441f8113cc3d7a26ea123bffcf4f7efadd8
Solyarka <97371336+Solyarka69@users.noreply.github.com>
2024-07-05 14:38:13 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
 const menuBtn = document.querySelector('.burger-btn');
 const menu = document.querySelector('.nav-list');
 const getQuoteBtn = document.querySelector('.nav-panel-quote-btn-mobile');
-const body = document.querySelector('body');
-const classNameList = 'nav-list-active';
+const body = document.body;
+const navListActiveClass = 'nav-list-active';
+const navListBtnActiveClass = 'nav-list-btn-active';
 
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('nav-list-active');
-    getQuoteBtn.classList.toggle('nav-list-btn-active');
-    if (menu.classList.contains(classNameList)){
-        body.style.overflowY = 'hidden';
-    } else{
-        body.style.overflowY = 'visible';
-    }
+function toggleMenu() {
+    const menuIsActive = menu.classList.toggle(navListActiveClass);
+    getQuoteBtn.classList.toggle(navListBtnActiveClass);
+    body.style.overflowY = menuIsActive ? 'hidden' : 'visible';
+}
 
-});
+menuBtn.addEventListener('click', toggleMenu);
