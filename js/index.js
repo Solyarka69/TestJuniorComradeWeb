ea66a974bac70d5b78ec62583c166be37172fcee
Solyarka <97371336+Solyarka69@users.noreply.github.com>
2024-07-05 14:45:15 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,13 +2,10 @@ const menuBtn = document.querySelector('.burger-btn');
 const menu = document.querySelector('.nav-list');
 const getQuoteBtn = document.querySelector('.nav-panel-quote-btn-mobile');
 const body = document.body;
-const navListActiveClass = 'nav-list-active';
-const navListBtnActiveClass = 'nav-list-btn-active';
+const classNameList = 'nav-list-active';
 
-function toggleMenu() {
-    const menuIsActive = menu.classList.toggle(navListActiveClass);
-    getQuoteBtn.classList.toggle(navListBtnActiveClass);
+menuBtn.addEventListener('click', () => {
+    const menuIsActive = menu.classList.toggle(classNameList);
+    getQuoteBtn.classList.toggle('nav-list-btn-active');
     body.style.overflowY = menuIsActive ? 'hidden' : 'visible';
-}
-
-menuBtn.addEventListener('click', toggleMenu);
+});
